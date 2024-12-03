ebdf59fffe249a1c4acb54caae8962aca299c033
dunningtonthomas <76410702+dunningtonthomas@users.noreply.github.com>
2024-12-03 12:07:43 -0700

    Add measurements plot

--- plotSim.m
+++ plotSim.m
@@ -1,15 +1,17 @@
-function plotSim(TOUT, YOUT, linespec)
+function plotSim(TOUT, XOUT, YOUT, linespec)
 %PLOTSIM Plots the 2D positions of the UGV and UAV and a 3x2 subplot of the
 %states over time
 % 
 % Inputs:
 %   TOUT -> time vector
-%   YOUT -> length(TOUT) by 6 state matrix, each column is a state over
+%   XOUT -> length(TOUT) by 6 state matrix, each column is a state
+%   YOUT -> length(TOUT) by 5 measurement matrix
 %   linespec -> linestyle of the plot
 %
 % Output:
 %   plot 1 -> 2D positions of UAV and UGV
 %   plot 2 -> 3x2 subplot of each state over time
+%   plot 3 -> 5x1 subplot of the measurements with no measurement noise
 
 % Define colors for better visualization
 ugv_color = [0, 0.4470, 0.7410]; % UGV color
@@ -17,9 +19,9 @@ uav_color = [0.8500, 0.3250, 0.0980]; % UAV color
 
 % Enhanced comparison plot
 figure(1);
-plot(YOUT(:,1), YOUT(:,2), 'LineWidth', 1.5, 'Color', ugv_color, 'DisplayName', 'UGV', 'linestyle', linespec)
+plot(XOUT(:,1), XOUT(:,2), 'LineWidth', 1.5, 'Color', ugv_color, 'DisplayName', 'UGV', 'linestyle', linespec)
 hold on;
-plot(YOUT(:,4), YOUT(:,5), 'LineWidth', 1.5, 'Color', uav_color, 'DisplayName', 'UAV', 'linestyle', linespec)
+plot(XOUT(:,4), XOUT(:,5), 'LineWidth', 1.5, 'Color', uav_color, 'DisplayName', 'UAV', 'linestyle', linespec)
 xlabel('East Position');
 ylabel('North Position');
 title('UGV vs UAV Position');
@@ -31,35 +33,35 @@ axis on;
 figure(2);
 sgtitle('Simulated States')
 subplot(3,2,1)
-plot(TOUT, YOUT(:,1), 'LineWidth', 1.5, 'Color', ugv_color, 'linestyle', linespec)
+plot(TOUT, XOUT(:,1), 'LineWidth', 1.5, 'Color', ugv_color, 'linestyle', linespec)
 ylabel('East Position');
 title('UGV East Position');
 grid on;
 hold on;
 
 subplot(3,2,2)
-plot(TOUT, YOUT(:,4), 'LineWidth', 1.5, 'Color', uav_color, 'linestyle', linespec)
+plot(TOUT, XOUT(:,4), 'LineWidth', 1.5, 'Color', uav_color, 'linestyle', linespec)
 ylabel('East Position');
 title('UAV East Position');
 grid on;
 hold on;
 
 subplot(3,2,3)
-plot(TOUT, YOUT(:,2), 'LineWidth', 1.5, 'Color', ugv_color, 'linestyle', linespec)
+plot(TOUT, XOUT(:,2), 'LineWidth', 1.5, 'Color', ugv_color, 'linestyle', linespec)
 ylabel('North Position');
 title('UGV North Position');
 grid on;
 hold on;
 
 subplot(3,2,4)
-plot(TOUT, YOUT(:,5), 'LineWidth', 1.5, 'Color', uav_color, 'linestyle', linespec)
+plot(TOUT, XOUT(:,5), 'LineWidth', 1.5, 'Color', uav_color, 'linestyle', linespec)
 ylabel('North Position');
 title('UAV North Position');
 grid on;
 hold on;
 
 subplot(3,2,5)
-plot(TOUT, YOUT(:,3), 'LineWidth', 1.5, 'Color', ugv_color, 'linestyle', linespec)
+plot(TOUT, XOUT(:,3), 'LineWidth', 1.5, 'Color', ugv_color, 'linestyle', linespec)
 ylabel('Heading Angle');
 title('UGV Heading Angle');
 xlabel('Time');
@@ -67,7 +69,7 @@ grid on;
 hold on;
 
 subplot(3,2,6)
-plot(TOUT, YOUT(:,6), 'LineWidth', 1.5, 'Color', uav_color, 'linestyle', linespec)
+plot(TOUT, XOUT(:,6), 'LineWidth', 1.5, 'Color', uav_color, 'linestyle', linespec)
 ylabel('Heading Angle');
 title('UAV Heading Angle');
 xlabel('Time');
@@ -76,6 +78,31 @@ hold on;
 
 % Global figure adjustments
 set(gcf, 'Position', [100, 100, 1200, 800]); % Adjust figure size
+
+
+% Measurement vector
+figure(3);
+sgtitle('Measurement')
+subplot(5,1,1)
+plot(TOUT, YOUT(:,1), 'linewidth', 1.5, 'Color', 'k', 'linestyle', linespec)
+ylabel('$\gamma_{ag}$ (rad)', 'Interpreter', 'latex')
+
+subplot(5,1,2)
+plot(TOUT, YOUT(:,2), 'linewidth', 1.5, 'Color', 'k', 'linestyle', linespec)
+ylabel('$\rho_{ga}$ (m)', 'Interpreter', 'latex')
+
+subplot(5,1,3)
+plot(TOUT, YOUT(:,3), 'linewidth', 1.5, 'Color', 'k', 'linestyle', linespec)
+ylabel('$\gamma_{ga}$ (rad)', 'Interpreter', 'latex')
+
+subplot(5,1,4)
+plot(TOUT, YOUT(:,4), 'linewidth', 1.5, 'Color', 'k', 'linestyle', linespec)
+ylabel('$\xi_{a}$ (m)', 'Interpreter', 'latex')
+
+subplot(5,1,5)
+plot(TOUT, YOUT(:,5), 'linewidth', 1.5, 'Color', 'k', 'linestyle', linespec)
+ylabel('$\eta_{a}$ (m)', 'Interpreter', 'latex')
+xlabel('Time (s)')
 
 
 end
