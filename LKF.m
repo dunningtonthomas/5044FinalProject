e0f52304708767c0a38811e98d65e0199577900b
owen444 <owcr8621@colorado.edu>
2024-12-03 20:24:14 -0700

    Timing Update

--- LKF.m
+++ LKF.m
@@ -2,9 +2,9 @@ function [x_est,sigma]= LKF(x_nom,u_nom,y_nom,y_actual,u_actual,Q,R,dt)
     % LKF This function is a Linearized Kalman Filter that returns the estimated state, measurements and covariance
     %
     % Inputs: 
-    %   x_nom       -> DT State Space
-    %   u_nom       -> Sensor Noise Covariance
-    %   y_nom       -> Process Noise Covariance
+    %   x_nom       -> Nominal State Trajectory
+    %   u_nom       -> Nominal Control Inputs
+    %   y_nom       -> Nominal Measured States
     %   y_actual    -> Actual measured state at all times
     %   u_actual    -> Actual control inputs at all times
     %   Q           -> Process Noise Covariance
@@ -22,10 +22,10 @@ function [x_est,sigma]= LKF(x_nom,u_nom,y_nom,y_actual,u_actual,Q,R,dt)
     Gamma = eye(size(x_nom,1),size(x_nom,1));
     for i = 1:n
         [A, B, C, D] = linearize(x_nom(:,i), u_nom(:,i));
-        [F,G,Omega,H] = eulerDiscretize(A,B,C,D,Gamma,dt);
+        [~,G,~,H] = eulerDiscretize(A,B,C,D,Gamma,dt);
         H_mat = [H_mat;H];
     end
-    % Initialize the filter using a batch lls to warm start the filter
+    % Initialize the filter using a batch lls to warm start the filter @k = 0
     R_mat = kron(eye(n), R);
     y_reshaped = reshape(y_actual', [], 1);
     xls  = (H_mat'*R_mat^-1*H_mat)^-1*H_mat'*R_mat^-1*y_reshaped;
@@ -36,21 +36,29 @@ function [x_est,sigma]= LKF(x_nom,u_nom,y_nom,y_actual,u_actual,Q,R,dt)
     dx_update(6) = mod(dx_update(6) + pi, 2*pi) - pi;
     du = zeros(size(G,2),1);
     dy_update = y_actual-y_nom;
+    x_est(:,1) = dx_update+x_nom(:,1);
+    sigma(:,1) = sqrt(diag(P_update));
+
     % Use the lineraized Kalman Filter to estimate the state
-    for i =1:n
+    for i =1:n-1
+        % Pure Predition uses F_k and G_k so calculate @ k
         [A, B, C, D] = linearize(x_nom(:,i), u_nom(:,i));
-        [F,G,Omega,H] = eulerDiscretize(A,B,C,D,Gamma,dt);
+        [F,G,~,~] = eulerDiscretize(A,B,C,D,Gamma,dt);
+        % Measurement Update uses H_K+1 so calculate H @ k+1
+        [A, B, C, D] = linearize(x_nom(:,i+1), u_nom(:,i+1));
+        [~,~,Omega,H] = eulerDiscretize(A,B,C,D,Gamma,dt);
         % Prediction Step
         % Estimate state perturbation
         dx = F*dx_update+G*du;
         P = F*P_update*F'+Omega*Q*Omega;
-        du = u_actual(:,i) - u_nom(:,i);
+        du = u_actual(:,i+1) - u_nom(:,i+1);
         % Measurement Update Step
         K = P*H'*(H*P*H'+R)^-1;
-        dx_update = dx +K*(dy_update(:,i)-H*dx);
+        dx_update = dx +K*(dy_update(:,i+1)-H*dx);
         P_update = (eye(size(P))-K*H)*P;
-        x_est(:,i) = dx_update+x_nom(:,i);
-        sigma(:,i) = sqrt(diag(P_update));
+        x_est(:,i+1) = dx_update+x_nom(:,i);
+        sigma(:,i+1) = sqrt(diag(P_update));
     end
-
+    x_est(3,:) = mod(x_est(3,:) + pi, 2*pi) - pi;
+    x_est(6,:) = mod(x_est(6,:) + pi, 2*pi) - pi;
 end
--- main_LKF.m
+++ main_LKF.m
@@ -25,7 +25,7 @@ u_nom = [u_ugv; u_uav];
 w = zeros(6,1);
 eomFunc = @(t, x)coopEOM(t, x, u_nom, w);
 x_init = x_nom;
-t_nom = (dt:dt:tspan(2))';
+t_nom = (0:dt:tspan(2))';
 [~, x_nom_mat] = ode45(eomFunc, t_nom, x_init, options);
 u_nom_mat = ones(length(t_nom), 4) .* u_nom';
 
@@ -45,10 +45,10 @@ rng(100);
 Q_true = Data.Qtrue;
 Sv_process = chol(Q_true,'lower');
 x_noise_mat = x_nom';
-t_noise = dt;
-for i=1:n_ind
+t_noise = 0;
+for i=1:n_ind+1
     % Define time step
-    ZOH_time = dt*[i i+1];
+    ZOH_time = dt*[i-1 i];
     % Generate AWGN for the time step
     q = randn(6,1);
     w =  Sv_process*q;
@@ -78,9 +78,10 @@ q = randn(5,length(t_noise));
 y_noise_mat =  y_noise_mat + (Sv_measurement*q)';
 
 %% Apply Linearized Kalman Filter
-[x_update,P_update]= LKF(x_nom_mat',u_nom_mat',y_nom_mat',y_noise_mat',u_nom_mat',Q_true,R_true,dt);
+[x_LKF,sigma]= LKF(x_nom_mat',u_nom_mat',y_nom_mat',y_noise_mat',u_nom_mat',Q_true,R_true,dt);
 
 
 %% Plotting
-plotSim(t_noise, x_noise_mat, y_noise_mat, '-')
-plotSim(t_nom, x_nom_mat, y_nom_mat, '--')
+% plotSim(t_noise, x_noise_mat, y_noise_mat, '--')
+plotSim(t_nom, x_nom_mat, y_nom_mat, '-')
+plotSim(t_noise(2:end), x_LKF(:,2:end)', y_noise_mat(2:end,:), '-.')
