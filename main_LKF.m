6498c3e0453f704b624145b3319b073810fc45d0
owen444 <owcr8621@colorado.edu>
2024-12-04 16:12:18 -0700

    Sensor noise time adjustment

--- main_LKF.m
+++ main_LKF.m
@@ -41,46 +41,10 @@ end
 
 
 %% Simulate Nonlinear Trajectory with Process Noise
-rng(100);
 Q_true = Data.Qtrue;
-Sv_process = chol(Q_true,'lower');
-x_noise_mat = x_nom';
-t_noise = 0;
-for i=1:n_ind+1
-    % Define time step
-    ZOH_time = dt*[i-1 i];
-    % Generate AWGN for the time step
-    q = randn(6,1);
-    w =  Sv_process*q;
-    % Set Initial Conditions
-    x_init = x_noise_mat(end,:);
-    % Function Handle for the nominal trajectory using the full nonlinear equations with noise
-    eomFunc = @(t, x)coopEOM(t, x, u_nom, w);
-    % Simulate over time step
-    [TOUT, x_noise_step] = ode45(eomFunc, ZOH_time, x_init, options);
-    x_noise_mat = [x_noise_mat;x_noise_step(end,:)];
-    t_noise = [t_noise;TOUT(end)];
-end
-
-% Angle wrapping
-x_noise_mat(:,3) = mod(x_noise_mat(:,3) + pi, 2*pi) - pi;
-x_noise_mat(:,6) = mod(x_noise_mat(:,6) + pi, 2*pi) - pi;
-
-% Calculate the measurements from the sensor model
-y_noise_mat = zeros(length(t_noise), 5);
-for i = 1:length(t_noise)
-    y_noise_mat(i,:) = sensors(x_noise_mat(i,:))';
-end
-% Add Measurement Noise 
 R_true = Data.Rtrue;
-Sv_measurement = chol(R_true,'lower');
-q = randn(5,length(t_noise));
-y_noise_mat =  y_noise_mat + (Sv_measurement*q)';
-
-plotSim(t_noise, x_noise_mat, y_noise_mat, '--')
-
 [time,x_noise_mat,y_noise_mat] = simulateNoise(x_nom,u_nom,Q_true,R_true,dt,1000);
-plotSim(t_noise, x_noise_mat, y_noise_mat, '-')
+plotSim(time, x_noise_mat, y_noise_mat, '-')
 
 
 %% Apply Linearized Kalman Filter
